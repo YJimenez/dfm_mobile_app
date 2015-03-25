a8ef136c8fdf1a2ba948f27ca7c584e5181fd5f5
kozlowskij <joshuamkozlowski@gmail.com>
2015-03-24 22:56:19 -1000

    init test feed, remove test value

--- client/js/src/main.jsx
+++ client/js/src/main.jsx
@@ -1,3 +1,4 @@
+Meteor.call('getFeed', 'http://delivery.digitalfirstmedia.com/ConvergencePublisher/?format=genericxml2spreed&uri=http://rss.denverpost.com/mngi/rss/CustomRssServlet/36/237705.xml', 'breaking');
 Meteor.subscribe('feeds');
 
 $(function(){
